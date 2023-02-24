ea48db3ebe8f3369a1c396c1ab12c3da589ec445
INT2ECALL <91420400+INT2ECALL@users.noreply.github.com>
2023-02-24 12:45:22 +0800

    Add files via upload

--- client.go
+++ client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"google.golang.org/grpc/encoding/gzip"
 	"log"
 	"time"
@@ -11,16 +12,15 @@ import (
 	_ "google.golang.org/grpc/encoding/gzip"
 )
 
-const (
-	address = "127.0.0.1:10000"
+var (
+	addr = flag.String("addr", "localhost:10000", "the address to connect to")
 )
 
 func main() {
-
-	//conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	flag.Parse()
 
 	//gzip
-	conn, err := grpc.Dial(address, grpc.WithInsecure(),grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name)))
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name)))
 
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -31,7 +31,19 @@ func main() {
 
 	ctx, cannel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cannel()
-	rsp, err := c.Login(ctx, &pb.LoginRequest{Username: "select a current_user", Password: "123"})
+
+	a := pb.Person{
+		Name:               "",
+		Id:                 0,
+		Email:              nil,
+		Phones:             nil,
+		WeightRecentMonths: nil,
+	}
+
+	b := make([]*pb.Person, 10)
+	b = append(b, &a)
+
+	rsp, err := c.Login(ctx, &pb.LoginRequest{Username: "select a current_user", Password: "123", People: b})
 	if err != nil {
 		log.Fatalf("could not login request: %v", err)
 	}
